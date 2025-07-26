dcf4b08f381ad269877ad9d6e5e17650470a6fcf
Maya Wright <mayagwright20@hotmail.com>
2025-07-27 00:10:22 +0100

    Change dictionary scrollview to flatlist

--- app/(dictionary)/index.tsx
+++ app/(dictionary)/index.tsx
@@ -5,9 +5,9 @@ import { useSearch } from '@/hooks/search-hook';
 import { usePersistStore } from '@/store/store';
 import { WORD } from '@/types';
 import {
+  FlatList,
   KeyboardAvoidingView,
   Platform,
-  ScrollView,
   StyleSheet,
   Text,
   View,
@@ -22,6 +22,53 @@ export default function Dictionary() {
 
   const { search, setSearch, filteredWords } = useSearch(WORDS);
 
+  const sortedWords = filteredWords.sort((a: WORD, b: WORD) => {
+    if (orderByEnglish) {
+      return a.english.localeCompare(b.english);
+    } else {
+      return a.creole.localeCompare(b.creole);
+    }
+  });
+
+  const renderItem = ({ item, index }: { item: WORD; index: number }) => {
+    const currentWord = orderByEnglish ? item.english : item.creole;
+    const nextWord = sortedWords[index + 1]
+      ? orderByEnglish
+        ? sortedWords[index + 1].english
+        : sortedWords[index + 1].creole
+      : null;
+
+    const showSeparator =
+      nextWord &&
+      currentWord.charAt(0).toUpperCase() !== nextWord.charAt(0).toUpperCase();
+
+    if (showSeparator) {
+      return (
+        <View key={`${index}-${item.english}-${item.creole}`}>
+          <DictionaryCard
+            onPressIsFavourite={() => toggleFavourite(item)}
+            isFavourited={item.isFavourited}
+            title={orderByEnglish ? item.english : item.creole}
+            description={orderByEnglish ? item.creole : item.english}
+          />
+          <Text style={styles.title}>{nextWord.charAt(0).toUpperCase()}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <DictionaryCard
+          onPressIsFavourite={() => toggleFavourite(item)}
+          isFavourited={item.isFavourited}
+          key={`${index}-${item.english}-${item.creole}`}
+          title={orderByEnglish ? item.english : item.creole}
+          description={orderByEnglish ? item.creole : item.english}
+        />
+      );
+    }
+  };
+
+  const ListHeaderComponent = () => <Text style={styles.title}>A</Text>;
+
   return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -32,68 +79,17 @@ export default function Dictionary() {
         input={search}
         setSearchInput={(searchInput) => setSearch(searchInput)}
       />
-      <ScrollView
+      <FlatList
+        data={sortedWords}
+        renderItem={renderItem}
+        keyExtractor={(item, index) =>
+          `${index}-${item.english}-${item.creole}`
+        }
         contentContainerStyle={styles.scrollView}
         keyboardShouldPersistTaps="handled"
         showsVerticalScrollIndicator={false}
-      >
-        <Text style={styles.title}>A</Text>
-        {filteredWords
-          .sort((a: WORD, b: WORD) => {
-            if (orderByEnglish) {
-              return a.english.localeCompare(b.english);
-            } else {
-              return a.creole.localeCompare(b.creole);
-            }
-          })
-          .map((item: WORD, index: number) => {
-            const sortedWords = filteredWords.sort((a: WORD, b: WORD) => {
-              if (orderByEnglish) {
-                return a.english.localeCompare(b.english);
-              } else {
-                return a.creole.localeCompare(b.creole);
-              }
-            });
-
-            const currentWord = orderByEnglish ? item.english : item.creole;
-            const nextWord = sortedWords[index + 1]
-              ? orderByEnglish
-                ? sortedWords[index + 1].english
-                : sortedWords[index + 1].creole
-              : null;
-
-            const showSeparator =
-              nextWord &&
-              currentWord.charAt(0).toUpperCase() !==
-                nextWord.charAt(0).toUpperCase();
-
-            if (showSeparator) {
-              return (
-                <View key={`${index}-${item.english}-${item.creole}`}>
-                  <DictionaryCard
-                    onPressIsFavourite={() => toggleFavourite(item)}
-                    isFavourited={item.isFavourited}
-                    title={orderByEnglish ? item.english : item.creole}
-                    description={orderByEnglish ? item.creole : item.english}
-                  />
-                  <Text style={styles.title}>
-                    {nextWord.charAt(0).toUpperCase()}
-                  </Text>
-                </View>
-              );
-            } else {
-              return (
-                <DictionaryCard
-                  onPressIsFavourite={() => toggleFavourite(item)}
-                  isFavourited={item.isFavourited}
-                  key={`${index}-${item.english}-${item.creole}`}
-                  title={orderByEnglish ? item.english : item.creole}
-                  description={orderByEnglish ? item.creole : item.english}
-                />
-              );
-            }
-          })}
-      </ScrollView>
+        ListHeaderComponent={ListHeaderComponent}
+      />
     </KeyboardAvoidingView>
   );
 }
@@ -111,7 +107,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 50,
-    fontWeight: FONT_WEIGHT.MEDIUM,
+    fontWeight: FONT_WEIGHT.MEDIUM as any,
     color: COLORS.BLUE,
     margin: MARGIN.XLARGE,
   },
